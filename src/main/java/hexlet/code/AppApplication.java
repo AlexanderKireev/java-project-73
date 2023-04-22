1eac81ab80bbbc568ccc3df54b739966fb7ff766
unknown <alexanderkir1980@gmail.com>
2023-04-23 00:43:00 +0300

    edit Docker

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -1,12 +1,14 @@
 package hexlet.code;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 // Добавляем поддержку авторизации через токен в свагер
 //@SecurityScheme(name = "javainuseapi", scheme = "bearer",
 // type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
